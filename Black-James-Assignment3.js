c842c880eaafed881c44107420149a557c74a6fe
JamesOBlack <jamesoblack@fullsail.edu>
2013-03-20 15:20:22 -0400

    Added object code.

--- Black-James-Assignment3.js
+++ Black-James-Assignment3.js
@@ -13,3 +13,22 @@ var artistInfo = {
         }
     ],    
 };
+
+var objNew = artistInfo;
+for (var key in objNew.artist) {
+    console.log("Artist: " + objNew.artist[key].artistFirstName);
+};
+
+// My Object
+var info = {
+    "name"       : "Caleb Hart",
+    "budget"     : 1000.00,
+    "costPerSong": 350.00,
+    "totalSongs"  : function(){ // Method: Accessor (Getter)
+        var totalSongs = info.budget / info.costPerSong;
+        return totalSongs;
+    }, // end totalSongs method;
+    "setBudget": function(newBudget){ // Method: Mutator (Setter)
+        info.budget = newBudget;
+    }
+    }; // end info object
